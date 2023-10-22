4fd5fe7326271d429a95cf5052de41612679ceb3
Johana Carrizo <johanacarrizo20@gmail.com>
2023-10-22 20:17:30 -0300

    Build: microservices client module was created

---
+++ microservices-client/src/test/java/com/microservices/client/MicroservicesClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
